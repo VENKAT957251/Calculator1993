0a92e94ba5839ba73f829ad5b4e3c192cb92f701
VENKAT957351 <venkatpavan2217@gmail.com>
2020-06-24 20:24:17 +0530

    my commit2

---
+++ src/com/maths/PracticeJava.java
@@ -0,0 +1,31 @@
+package com.maths;
+
+public class PracticeJava extends Calculator{
+	
+	
+	  public static void add() {
+	  
+	 int a=7; int b=7; int add=a+b; System.out.println("addition is "+add); }
+	 
+	  
+	  public void add1() {
+		  
+			 int a=7; int b=10; int add=a+b; System.out.println("addition is "+add); }
+	  
+	  
+	public static void main(String[] args) {
+		
+add();
+
+PracticeJava ik = new PracticeJava();
+Calculator kh = new Calculator();
+ik.add1();
+int fgh=kh.sub(9,7);
+int sub1=kh.sub(6,7);
+ 
+
+System.out.println("sub is = "+fgh+" sub1 is "+sub1);
+
+	}
+
+}
